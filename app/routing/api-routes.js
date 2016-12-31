5ad8054589d3d910ec018962dccd53b75fb78510
David Bethelmy <bethelmyd@gmail.com>
2016-12-31 02:20:19 -0500

    fixed bug in routing

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -7,18 +7,18 @@ var fs = require("fs");
 var friends = require("../data/friends.json");
 
 module.exports.friends = function(req, res) {
-  var chosen = req.params.whatToDo;
+//   var chosen = req.params.whatToDo;
 
-  if (chosen) {
-    console.log(chosen);
-    if(chosen == "list")
-    {
+//   if (chosen) {
+//     console.log(chosen);
+//     if(chosen == "list")
+//     {
       res.json(friends);
-    }
-    else{
-      res.json(null);
-    }
-  }
+//     }
+//     else{
+//       res.json(null);
+//     }
+//   }
 };
 
 module.exports.addFriend =function(req, res) {
